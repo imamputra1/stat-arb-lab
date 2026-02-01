e79f2698c6cbc281816d08a20d0c14e279788535
imamputra1 <a.i.syahputra415@gmail.com>
2026-02-01 12:23:06 +0700

    FEAT: add generate features hash for unit storage in the metadata registy module

--- research/processing/storage/metadata_registry.py
+++ research/processing/storage/metadata_registry.py
@@ -1,4 +1,6 @@
 import logging
+import json
+import hashlib
 from typing import Dict, TYPE_CHECKING
 from pathlib import Path
 
@@ -25,4 +27,24 @@ class MetadataRegisty:
         self.storage_path.mkdir(parents=True, exist_ok=True)
         logger.debug(f"MetadataRegisty initialized at: {self.storage_path}")
 
-    
+    def generate_feature_hash(self, config_str: Dict[str, str]) -> str:
+
+        try:
+            config_str = json.dumps(config, sort_keys=True, default=str)
+
+            if config_str in self._cached_hashes:
+                return self._cached_hashes[config_str]
+
+            hash_job = hashlib.sha256(config_str.encode())
+            feature_hash = hash_job
+
+            if len(self._cached_hashes) < 100:
+                self._cached_hashes[config_str] = feature_hash
+
+            return feature_hash
+
+        except Exception as e:
+            logger.error(f"Feature Hash Generation Failed: {str(e)}")
+            return ValueError("Failed to Generate Feature Hash")
+
+
