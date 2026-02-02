62130c75dcfdd6d389a9dce8274160cbd9956db2
imamputra1 <a.i.syahputra415@gmail.com>
2026-02-02 20:26:23 +0700

    FEAT: Add functions execute pair arbitrage & generate result(privet)

--- research/strategy/pipeline.py
+++ research/strategy/pipeline.py
@@ -4,6 +4,7 @@ import sys
 import argparse
 from datetime import datetime
 from typing import Dict, List, Any, Optional, Tuple
+from pandas import DataFrame
 import polars as pl
 from pathlib import Path
 
@@ -67,4 +68,62 @@ class StrategyPipeline:
         self.engine = None
         logger.info(f"Pipeline Ignited | silver_path")
 
-    def execute_pair_arbitrage()
+    def execute_pair_arbitrage(
+        self,
+        target_symbol: str,
+        anchore_symbol: str,
+        start_date: str,
+        end_date: str,
+        save_results: bool = True
+    ) -> Result[Dict[str, Any], str]:
+        try:
+            logger.info(f"EXECUTE | Pair{target_symbol}-{anchore_symbol} | Periods: {start_date} to {end_date}")
+
+            self.engine = create_backtest_engine(
+                data_loader=self.loader,
+                strategy_model=self.model,
+                warmup_days=self.warmup_days,
+                entry_threshold=self.entry_threshold,
+                exit_threshold=self.exit_threshold
+            )
+            
+            backtest_res = self.engine.run(
+                start_date=start_date,
+                end_date=end_date,
+                target_asset=target_symbol,
+                anchore_asset=anchore_symbol
+            )
+
+            if backtest_res.is_err():
+                return Err(f"Simulation Failure: {backtest_res.error}")
+
+            result_df = backtest_res.unwrap()
+
+            self._generate_report(result_df, target_symbol)
+
+            if save_results:
+                save_res = self.save_results(result_df, target_symbol, anchore_symbol)
+                if save_res.is_ok():
+                    logger.info(f"STORAGE | Result saved to {save_res.unwrap()}")
+
+            return Ok({"status": "completed", "rows": result_df.height})
+
+        except Exception as e:
+            logger.error(f"Pipeline Crash: {str(e)", exc_info=True})
+            return Err(f"Pipeline Error: {str(e)}")
+
+    def _generate_report(self, df: pl.DataFrame, target: str):
+        trades = df.filter(pl.col("posisition").diff() !=0).height
+        final_pnl = df["cum_pnl"].tail(1)[0] if "cum_pnl" in df.columns else 0.0
+
+
+        print("\n" + "="*50)
+        print(f" STRATEGY REPORT: {target}-BTC")
+        print("-" * 50)
+        print(f" Simulation Period: {df['timestamp'].min()} - {df['timestamp'].max()}")
+        print(f" Total Samples   : {df.height:,}")
+        print(f" Total Trades    : {trades:,}")
+        print(f" Final Cum PnL   : {final_pnl:.6f}")
+        print("="*50 + "\n")
+
+
